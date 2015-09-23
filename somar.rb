abeeca2584bef133c2478b9157c80dbb5e06efbf
Rhuan Carlos <rhuancarlosg12@gmail.com>
2015-09-22 19:43:33 -0700

    Somando numeros com ruby puro"

---
+++ somar.rb
@@ -0,0 +1,12 @@
+puts "================ CALCULANDO NUMEROS ================"
+puts
+puts "Digite um numero"
+num1 = gets.chomp.to_i
+puts "Digite mais um numero"
+num2 = gets.chomp.to_i
+
+resultado = num1 + num2
+puts "Resultado............... #{resultado}"
+puts
+puts
+puts "by: rhuanOliver"
